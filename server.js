b3378226f80bdd1f8e0fa6e79820290acc8cfb4a
Bruno Ravanello <bravanello89@gmail.com>
2025-10-13 18:15:44 -0300

    ok

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 const schema = require("./schema");
 const { verifyToken } = require("./token");
+const expressPlayground = require("graphql-playground-middleware-express").default;
 
 const app = express();
 const port = 4000;
@@ -12,10 +13,6 @@ mongoose.connect(`mongodb://localhost:${mongoPort}/graphql`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-mongoose.connection.once("open", () =>
-  console.log("Connected to MongoDB")
-);
 
 app.use(
   "/graphql",
@@ -34,12 +31,15 @@ app.use(
 
     return {
       schema,
-      graphiql: true,
+      graphiql: false,
       context: { user },
     };
   })
 );
 
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/graphql`);
+  // console.log(`Server running at http://localhost:${port}/graphql`);
+  console.log(`GraphQL Playground at http://localhost:${port}/playground`);
 });
